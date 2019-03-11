7860b3e1379facd7bfe8e28fca8eb85353e41c04
Daniel Saxén <da_sax_e@hotmail.com>
2019-03-11 03:39:56 +0200

    Added text field for MAC

--- app/src/main/java/com/example/ds/internetprotocols_bleapp/MainActivity.java
+++ app/src/main/java/com/example/ds/internetprotocols_bleapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ds.internetprotocols_bleapp;
 
+import android.Manifest;
 import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -12,8 +13,10 @@ import android.bluetooth.le.ScanResult;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Message;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -22,6 +25,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
     BluetoothAdapter bAdapter;
 
     private Button button;
-    private  TextView readStatus;
+    private TextView readStatus;
+    private EditText macAddress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,17 +61,26 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button);
         readStatus = (TextView) findViewById(R.id.readStatus);
-        mBTArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1);
+        macAddress = (EditText) findViewById(R.id.editText);
 
+
+        //initialize Location
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
 
         //initialize Bluetooth
         final BluetoothManager bManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bAdapter = bManager.getAdapter();
 
+
         //enable Bluetooth
         if (bAdapter == null || !bAdapter.isEnabled()) {
             Intent enableBIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBIntent, REQUEST_ENABLE_BT);
+        }
+
+        if (bAdapter == null){
+            System.out.println("No support for Bluetooth");
+            return;
         }
         mHandler = new Handler(){
             public void handleMessage(android.os.Message msg){
@@ -83,9 +97,56 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        // Get the device MAC address, which is the last 17 chars in the View
+        final String address = macAddress.getText().toString();
+        // Spawn a new thread to avoid blocking the GUI one
+
         button.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v){
+                final String address = macAddress.getText().toString();
+                if(address == null){
+                    return;
+                }
+
+                new Thread()
+                {
+                    public void run() {
+                        boolean fail = false;
+
+                        BluetoothDevice device = bAdapter.getRemoteDevice(address);
+
+                        try {
+                            mBTSocket = createBluetoothSocket(device);
+                        } catch (IOException e) {
+                            fail = true;
+                            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
+                        }
+                        // Establish the Bluetooth socket connection.
+                        try {
+                            mBTSocket.connect();
+                        } catch (IOException e) {
+                            try {
+                                fail = true;
+                                mBTSocket.close();
+                                mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
+                                        .sendToTarget();
+                            } catch (IOException e2) {
+                                //insert code to deal with this
+                                Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        if(fail == false) {
+                            connThread = new ConnectedThread(mBTSocket);
+                            connThread.start();
+
+                            mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, "ewe")
+                                    .sendToTarget();
+                        }
+                    }
+                }.start();
+
                 if(connThread != null){
                     connThread.write("hi there");
                 }
@@ -103,58 +164,6 @@ public class MainActivity extends AppCompatActivity {
                 mBTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 mBTArrayAdapter.notifyDataSetChanged();
             }
-        }
-    };
-    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
-
-            if(!bAdapter.isEnabled()) {
-                Toast.makeText(getBaseContext(), "Bluetooth not on", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-            // Get the device MAC address, which is the last 17 chars in the View
-            String info = ((TextView) v).getText().toString();
-            final String address = info.substring(info.length() - 17);
-            final String name = info.substring(0,info.length() - 17);
-
-            // Spawn a new thread to avoid blocking the GUI one
-            new Thread()
-            {
-                public void run() {
-                    boolean fail = false;
-
-                    BluetoothDevice device = bAdapter.getRemoteDevice(address);
-
-                    try {
-                        mBTSocket = createBluetoothSocket(device);
-                    } catch (IOException e) {
-                        fail = true;
-                        Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
-                    }
-                    // Establish the Bluetooth socket connection.
-                    try {
-                        mBTSocket.connect();
-                    } catch (IOException e) {
-                        try {
-                            fail = true;
-                            mBTSocket.close();
-                            mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
-                                    .sendToTarget();
-                        } catch (IOException e2) {
-                            //insert code to deal with this
-                            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    if(fail == false) {
-                        connThread = new ConnectedThread(mBTSocket);
-                        connThread.start();
-
-                        mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, name)
-                                .sendToTarget();
-                    }
-                }
-            }.start();
         }
     };
 
